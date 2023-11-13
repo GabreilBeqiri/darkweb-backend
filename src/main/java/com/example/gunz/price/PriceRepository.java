bc3ccea922b18914ae24612854b04b5f07439841
GabrielBeqiri <beqirigabriel10@gmail.com>
2023-11-13 11:17:36 +0100

    Sales

--- src/main/java/com/example/gunz/price/PriceRepository.java
+++ src/main/java/com/example/gunz/price/PriceRepository.java
@@ -12,4 +12,12 @@ public interface PriceRepository extends JpaRepository<Price, Long> {
 
     @Query(value = "Select * from price u where u.id = ?1 ",nativeQuery = true)
     public Price findPriceById(@Param("id") Integer id);
+
+    @Query(value = "Select sum(u.sales) from price u ",nativeQuery = true)
+    public Integer findsumm() ;
+
+    @Query(value = "SELECT sum(u.sales) FROM gunz.price u\n" +
+            " where CAST(u.created_at AS DATE) = CAST( curdate() AS DATE); ",nativeQuery = true)
+    public Integer findstodaysumm() ;
+
 }
